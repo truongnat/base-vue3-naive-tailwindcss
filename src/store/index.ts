a73cbdcfd7150b384620d88a2cb1dd0c17f16873
TRUONGDEV\truon <truongdq.dev@gmail.com>
2022-02-01 14:12:47 +0700

    update plugins vuex store

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,10 +2,29 @@ import { AppState, Store } from '@/@types';
 import { isDev, isProd } from '@/constants';
 import { createStore, createLogger } from 'vuex';
 import { rootModule } from './modules';
+import createPersistedState from 'vuex-persistedstate';
+import _ from 'lodash';
+
+const myPluginWithSnapshot = (store: Store) => {
+  let prevState = _.cloneDeep(store.state);
+  store.subscribe((mutation, state) => {
+    const nextState = _.cloneDeep(state);
+
+    // compare `prevState` and `nextState`...
+
+    // save state for next mutation
+    prevState = nextState;
+  });
+  // @ts-ignore
+  window.__temp__prev__state = prevState;
+};
+
+const plugins = !isProd ? [createLogger(), myPluginWithSnapshot] : [];
+plugins.push(createPersistedState({ storage: window.sessionStorage }));
 
 export const store = createStore<AppState>({
   devtools: isDev,
-  plugins: !isProd ? [createLogger()] : [],
+  plugins,
   modules: {
     rootState: rootModule,
   },
